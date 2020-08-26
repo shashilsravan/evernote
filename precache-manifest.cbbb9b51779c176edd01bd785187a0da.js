a828cb86659cd1ea2c58d1e944cd5370a0948f64
shashilsravan <shashilsravan.ss.ss@gmail.com>
2020-08-26 12:46:37 +0530

    Updates

---
+++ precache-manifest.cbbb9b51779c176edd01bd785187a0da.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "122bc085b21ccdef12fccbcb5f4d0983",
+    "url": "/evernote/index.html"
+  },
+  {
+    "revision": "1df7ad4af1b44d3e67d6",
+    "url": "/evernote/static/css/main.65553a91.chunk.css"
+  },
+  {
+    "revision": "0f4814aac348630d41c2",
+    "url": "/evernote/static/js/2.092330c2.chunk.js"
+  },
+  {
+    "revision": "1df7ad4af1b44d3e67d6",
+    "url": "/evernote/static/js/main.79ef0657.chunk.js"
+  },
+  {
+    "revision": "e02e0c106e58bb54e1bc",
+    "url": "/evernote/static/js/runtime~main.4484c875.js"
+  }
+]);
